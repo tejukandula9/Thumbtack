8804e67d8bdced616c746031917880922c5eb360
Prachi <64607735+pbambarkar29@users.noreply.github.com>
2022-02-01 21:44:25 -0800

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier,DecisionTreeRegressor, plot_tree
 from sklearn import tree
